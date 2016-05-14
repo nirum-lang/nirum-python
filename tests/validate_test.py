b52fa24dfdf7b10cf4ef80cb63af36aba932e59f
Hyojun Kang <admire9@gmail.com>
2016-05-14 15:09:13 +0900

    Fix test_validate_boxed_type

--- tests/validate_test.py
+++ tests/validate_test.py
@@ -3,8 +3,7 @@ from pytest import raises
 from nirum.validate import validate_boxed_type
 
 
-def test_validate_boxed_type(fx_offset, fx_boxed_type):
-    assert validate_boxed_type(fx_offset, float)
-    wrong_boxed_type = fx_boxed_type('hello')
+def test_validate_boxed_type():
+    assert validate_boxed_type(3.14, float)
     with raises(TypeError):
-        validate_boxed_type(wrong_boxed_type, float)
+        validate_boxed_type('hello', float)
